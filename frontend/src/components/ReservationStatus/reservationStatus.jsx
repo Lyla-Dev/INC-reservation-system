a630527078764a472c55e6c3c733b237cc0fbb26
Minkyeong-Choi <minkyeong.choi@outlook.com>
2025-05-27 15:38:18 +0900

    front/adapt font, correct alignment

--- frontend/src/components/ReservationStatus/reservationStatus.jsx
+++ frontend/src/components/ReservationStatus/reservationStatus.jsx
@@ -3,6 +3,10 @@ import ReservationInfoBox from './reservationStatusBox';
 import ReservationStatusImage from '../../assets/reservationStatusImage.jpg'
 
 function ReservationDisplay() {
+  const handleCancel = () => { // 하나의 공통 핸들러를 정의하거나, 각 박스마다 정의
+    console.log("Reservation cancelled from ReservationDisplay!");
+  };
+
   return (
     <div style={{
       display: 'flex',
@@ -20,22 +24,44 @@ function ReservationDisplay() {
         marginBottom: '40px'
       }}></div>
 
-      {/* 첫 번째 예약 정보 */}
       <ReservationInfoBox
         date="2025.05.20"
-        time="LUNCH"
+        time="DINNER"
         phoneNumber="010-1234-5678"
-        numberOfPeople={4}
+        guests={1}
         tableType="window"
+        cancellationDeadline="25.10.04 12pm"
+        onCancel={handleCancel}
       />
 
-      {/* 두 번째 예약 정보 */}
       <ReservationInfoBox
         date="2025.05.20"
         time="LUNCH"
         phoneNumber="010-1234-5678"
-        numberOfPeople={4}
+        guests={4}
         tableType="window"
+        cancellationDeadline="25.10.04 12pm"
+        onCancel={handleCancel}
+      />
+
+      <ReservationInfoBox
+        date="2025.05.20"
+        time="DINNER"
+        phoneNumber="010-1234-5678"
+        guests={4}
+        tableType="window"
+        cancellationDeadline="25.10.04 12pm"
+        onCancel={handleCancel}
+      />
+
+      s<ReservationInfoBox
+        date="2025.05.20"
+        time="LUNCH"
+        phoneNumber="010-1234-5678"
+        guests={4}
+        tableType="window"
+        cancellationDeadline="25.10.04 12pm"
+        onCancel={handleCancel}
       />
     </div>
   );
