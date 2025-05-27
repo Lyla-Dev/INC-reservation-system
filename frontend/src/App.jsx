5883679bdefa3c3d6a81cba6a30e21d80992bec5
240nayoung <lny0912@gmail.com>
2025-05-27 14:47:36 +0900

    메인
    # Conflicts:
    #	frontend/src/App.css
    #	frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
+
 import MainPage from './components/MainPage/mainPage';
+import PageLayout from './components/PageLayout/pageLayout';
+import SignUp from './components/SignUp/signUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LogIn from './components/LogIn/logIn'
 
 function App() {
+  
   return (
     <Router>
       <Routes>
-        <Route path="/mainPage" element={<MainPage />} />
+        <Route path="/mainpage" element={<MainPage />} />
       </Routes>
     </Router>
   );
