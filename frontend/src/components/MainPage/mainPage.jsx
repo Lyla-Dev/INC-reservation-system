17b8889fee11b7a126885dcefbbeb2595abd1fe5
240nayoung <lny0912@gmail.com>
2025-05-27 13:16:46 +0900

    메인페이지

--- frontend/src/components/MainPage/mainPage.jsx
+++ frontend/src/components/MainPage/mainPage.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 const MainPage = () => {
   const boxStyle = {
     width: '300px',
-    height: '300px',
-    backgroundColor: ' rgba(150, 178, 240, 0.84)',
+    height: '400px',
+    backgroundColor: 'rgba(130, 163, 192, 0.86)',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
     fontWeight: 'bold',
     fontSize: '20px',
-    borderRadius: '12px',
+    borderRadius: '0px',
     boxShadow: '0 0 0 rgba(0,0,0,0)',
     transition: 'all 0.2s',
     cursor: 'pointer'
@@ -20,7 +20,7 @@ const MainPage = () => {
     ...boxStyle,
     transform: 'scale(1.05)',
     boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.2)',
-    backgroundColor: ' rgb(115, 157, 220)'
+    backgroundColor: '#547792'
   };
 
   const [hoveredIndex, setHoveredIndex] = React.useState(null);
