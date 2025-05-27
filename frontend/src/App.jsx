61ab76bc1d64a370ccf273bd26fd28b3021e6cf6
Minkyeong-Choi <minkyeong.choi@outlook.com>
2025-05-27 14:40:54 +0900

    front/adapt font

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,21 @@ import PageLayout from './components/PageLayout/pageLayout';
 import SignUp from './components/SignUp/signUp';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LogIn from './components/LogIn/logIn'
+import './App.css';
 
 function App() {
-  
   return (
-    <Router>
+    <div>
+      {/* 폰트 사용 예시 */}
+      <p style={{fontFamily: 'title', fontSize: '30px'}}>안녕하세요, 돋움(로고/제목) 폰트입니다!</p>
+      <p style={{fontFamily: 'content'}}>안녕하세요, 바탕체(본문 내용) 폰트입니다!</p>
+
+    {/* <Router>
       <Routes>
         <Route path="/mainpage" element={<MainPage />} />
       </Routes>
-    </Router>
+    </Router> */}
+    </div>
   );
 }
 
